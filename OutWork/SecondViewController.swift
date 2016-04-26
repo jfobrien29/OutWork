9012f1cc0fb6525d26a3fd6666fca40b965f0da3
jfobrien29 <jfobrien@princeton.edu>
2016-04-26 02:15:38 -0400

    Initial Commit

---
+++ OutWork/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  OutWork
+//
+//  Created by Jack O'Brien on 4/26/16.
+//  Copyright (c) 2016 OutWorkDev. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
