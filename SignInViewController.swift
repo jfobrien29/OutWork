0316d5f16fcd79f6328e647df25f75858e3909b4
jfobrien29 <jfobrien@princeton.edu>
2016-04-26 23:39:53 -0400

    Minor Changes to StoryBoard, Major additions to SignInViewController.swift

--- SignInViewController.swift
+++ SignInViewController.swift
@@ -11,13 +11,22 @@ import Firebase
 
 class SignInViewController: UIViewController {
     
+    // Properties
+    @IBOutlet weak var EmailSignIn: UITextField!
+    @IBOutlet weak var PasswordSignIn: UITextField!
+    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var ResetPasswordButton: UIButton!
+    @IBOutlet weak var SignUpButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Create a reference to a Firebase location
         var myRootRef = Firebase(url:"https://outwork.firebaseio.com")
         // Write data to Firebase
-        myRootRef.setValue("Do you have data? You'll love Firebase. Please Work!")
+        var date = NSDate()
+        myRootRef.setValue(["Sample Data": NSDate().description])
     }
     
     override func didReceiveMemoryWarning() {
@@ -25,6 +34,37 @@ class SignInViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // Below: Actions!
+    
+    @IBAction func pressResetPassword(sender: UIButton) {
+        let ref = Firebase(url: "https://outwork.firebaseio.com")
+        ref.resetPasswordForUser(EmailSignIn.text, withCompletionBlock: { error in
+            
+            if error != nil {
+                // There was an error processing the request
+            } else {
+                // Password reset sent successfully
+            }
+        })
+        
+    }
+    
+    @IBAction func pressSignUp(sender: UIButton) {
+        var myRootRef = Firebase(url:"https://outwork.firebaseio.com")
+        
+        myRootRef.createUser(EmailSignIn.text, password: PasswordSignIn.text,
+            withValueCompletionBlock: { error, result in
+                if error != nil {
+                    // Error Occured
+                    println("Error Creating account")
+                    
+                } else {
+                    // Successful Creation
+                    let uid = result["uid"] as? String
+                    println("Successfully created user account with uid: \(uid)")
+                }
+        })
+    }
     
 }
 
